08b2e80af18360f25751b5517e3fd0c67c90bcce
HuangJue <2046682838@qq.com>
2022-11-29 19:54:00 +0800

    first-commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/*************************************************************************
+    > File Name: Hello.java
+    > Author: ma6174--when i learned the script thorought,i will change the name HAHAHAH
+    > Mail: ma6174@163.com 
+    > Created Time: 2022年11月29日 星期二 19时45分53秒
+ ************************************************************************/
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello,Git");
+	}
+}
