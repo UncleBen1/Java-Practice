122bfe50d640f1032d8e09f20a4f980b295c980a
HuangJue <2046682838@qq.com>
2022-11-29 22:41:22 +0800

    add

---
+++ First/Testadd1.java
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: Testadd1.java
+    > Author: ma6174--when i learned the script thorought,i will change the name HAHAHAH
+    > Mail: ma6174@163.com 
+    > Created Time: 2022年11月29日 星期二 22时39分41秒
+ ************************************************************************/
+class Testadd1{
+	public static void main(String[] args){
+		var a=7;
+		var b=9;
+		System.out.println(a+b);
+	}
+}
