cdbbf21b1163d0ae27049bb637c51d9db41baeba
HuangJue <2046682838@qq.com>
2022-11-29 23:03:19 +0800

    new

---
+++ NewFileTest/Test.java
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: Test.java
+    > Author: ma6174--when i learned the script thorought,i will change the name HAHAHAH
+    > Mail: ma6174@163.com 
+    > Created Time: 2022年11月29日 星期二 23时02分26秒
+ ************************************************************************/
+
+public Test{
+	public static void main(String[] args){
+		System.out.print("New file");
+	}
+}
